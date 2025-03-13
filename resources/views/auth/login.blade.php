5be0d1c836ef4713dd89e2000cb2fd8d5c18f0c4
Miguel03br <miguel.costa15@aluno.ce.gov.br>
2025-03-13 11:51:56 -0300

    Início da tela de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,10 +9,10 @@
 <body>
     <form action="">
         <div class="border rounded-xl justify-self-center text-center w-110 h-140 mt-40 shadow-xl">
-            <h2>Login</h2>
-            <input type="text" placeholder="Email" class="border rounded-xl flex justify-self-center">
-            <input type="text" placeholder="Senha" class="border rounded-xl flex justify-self-center">
-            <button class="border rounded-xl flex justify-self-center">Entrar</button>
+            <h2 class="font-bold text-6xl mt-13">Login</h2>
+            <input type="text" placeholder="Email" class="border rounded-xl flex justify-self-center mt-36 w-80 h-13 pl-2">
+            <input type="text" placeholder="Senha" class="border rounded-xl flex justify-self-center mt-10 w-80 h-13 pl-2">
+            <button class="text-center border w-80 h-13 rounded-xl mt-10 cursor-pointer">Entrar</button>
         </div>
     </form>
 </body>
