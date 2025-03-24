c70d7ec498e85369c7ccc1851757e371fb1b2207
Miguel03br <miguel.costa15@aluno.ce.gov.br>
2025-03-24 14:11:58 -0300

    InicioDoLogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,10 @@
-<<<<<<< HEAD
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 
 <form action="" class="">
@@ -8,31 +14,11 @@
         class="border justify-self-center w-30 h-30 mt-10 rounded-2xl "></div>
         
 
-        <input type="email" placeholder="Seu email" class=" flex justify-self-center w-80 h-13 rounded-xl mt-20 pl-4 text-md font-semibold bg-slate-50">
+        <button class="text-center justify-self-center mt-30 w-80 h-13 rounded-xl cursor-pointer bg-neutral-800 text-slate-50 text-2xl" style="font-family: nunito">ENTRAR</button>
 
-        <input type="password" placeholder="Sua senha" class=" flex justify-self-center w-80 h-13 rounded-xl mt-10 pl-4 text-md font-semibold bg-slate-50">
+        <button class="text-center justify-self-center mt-10 w-80 h-13 rounded-xl cursor-pointer bg-neutral-800 text-slate-50 text-2xl" style="font-family: nunito">REGISTRAR-SE</button>
 
-        <button class="text-center justify-self-center mt-8 w-30 h-13 rounded-xl cursor-pointer bg-orange-500 font-semibold text-slate-50">Entrar</button>
+        <a class="mt-14 cursor-pointer flex justify-self-center" style="font-family: K2D">Ajuda</a>
     </div>
 </form>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-    <title>Document</title>
-</head>
-<body>
-    <form action="">
-        <div class="border rounded-xl justify-self-center text-center w-110 h-140 mt-40 shadow-xl">
-            <h2 class="font-bold text-6xl mt-13">Login</h2>
-            <input type="text" placeholder="Email" class="border rounded-xl flex justify-self-center mt-36 w-80 h-13 pl-2">
-            <input type="text" placeholder="Senha" class="border rounded-xl flex justify-self-center mt-10 w-80 h-13 pl-2">
-            <button class="text-center border w-80 h-13 rounded-xl mt-10 cursor-pointer">Entrar</button>
-        </div>
-    </form>
-</body>
-</html>
->>>>>>> 5be0d1c836ef4713dd89e2000cb2fd8d5c18f0c4
+
