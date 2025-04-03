0ee492a54534871b4fd14c3c8526932ce939bec9
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-03 10:45:47 -0300

    Design do Modo de cadastros

--- resources/views/modos_de_cadastro.blade.php
+++ resources/views/modos_de_cadastro.blade.php
@@ -15,15 +15,15 @@
 </head>
 <body>
     <form action="" class="">
-    <div class="rounded-xl justify-self-center mt-40 text-center w-107 h-158 shadow-xl bg-neutral-100 pt-12 border">
+    <div class="rounded-xl justify-self-center mt-40 text-center w-107 h-158 shadow-xl bg-neutral-100 pt-12">
 
         <div class="border justify-self-center w-30 h-30 rounded-2xl font-bold"></div>
         
         <h2 class="justify-self-center text-2xl" style="font-family: K2D">Bii-Bi Express</h2>
 
-        <div class="border w-85 h-70 justify-self-center shadow-2xl rounded-xl mt-25">
+        <div class="w-85 h-70 justify-self-center shadow-2xl rounded-xl mt-25 bg-neutral-100 pt-4">
             
-            <p style="font-family: Nunito" class="mt-4 font-bold">Como deseja se cadastrar?</p>
+            <p style="font-family: Nunito" class="font-bold">Como deseja se cadastrar?</p>
 
             <button class="text-center justify-self-center mt-10 w-70 h-11 rounded-xl cursor-pointer bg-neutral-800 text-slate-50 text-2xl" style="font-family: nunito">USUÁRIO</button>
 
@@ -33,7 +33,7 @@
 
         </div>
 
-        <a class="mt-6 cursor-pointer flex justify-self-center" style="font-family: K2D">Ajuda</a>
+        <a class="mt-4 cursor-pointer flex justify-self-center" style="font-family: K2D">Ajuda</a>
 
     </div>
     </form>
