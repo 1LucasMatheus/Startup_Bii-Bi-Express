7e4d491be541b005273fc098f5d2d3733cef4e27
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-03 12:53:12 -0300

    Cadastro empresa e usuário/entregador

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,13 +10,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>Document</title>
 </head>
 <body>
     <?php
 
+    <!-- Cadastro do usuário/entregador -->
     <form action="" class="">
     <div class="rounded-xl justify-self-center mt-13 text-center w-112 h-197 shadow-xl bg-neutral-100 pt-12">
 
@@ -26,7 +26,7 @@
 
         <div class="rounded-xl shadow-xl w-90 h-137 justify-self-center mt-4 pt-2">
 
-            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">LOGIN</h2>
+            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">REGISTRAR-SE</h2>
 
             <p style="font-family: K2D" class="mt-10 flex pl-6 text-lg font-bold">E-mail:</p>
             <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="Usuario@gmail.com">
@@ -40,7 +40,7 @@
             <p style="font-family: K2D" class="mt-2 flex pl-6 text-lg font-bold">Confirme sua senha:</p>
             <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="***********">
 
-            <button class="w-40 h-12 border mt-6 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">ENTRAR</button>
+            <button class="w-40 h-12 border mt-6 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">CONTINUAR</button>
 
             <a class="cursor-pointer flex justify-self-center font-bold text-orange-400 mt-2" style="font-family: K2D">Já possui uma conta? Entrar</a>
         </div>
@@ -48,25 +48,28 @@
     </div>
     </form>
 
-
+    <!--- Cadastro da empresa -->
     <form action="" class="">
-    <div class="rounded-xl justify-self-center mt-13 text-center w-112 h-189 shadow-xl bg-neutral-100 pt-12">
+    <div class="rounded-xl justify-self-center mt-13 text-center w-112 h-197 shadow-xl bg-neutral-100 pt-12">
 
         <div class="border justify-self-center w-30 h-30 rounded-2xl font-bold"></div>
         
         <h2 class="justify-self-center text-2xl" style="font-family: K2D">Bii-Bi Express</h2>
 
-        <div class="rounded-xl shadow-xl w-90 h-107 justify-self-center mt-25 pt-2">
+        <div class="rounded-xl shadow-xl w-90 h-117 justify-self-center mt-25 pt-2">
 
-            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">INFORMAÇÃO DO VEÍCULO</h2>
+            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">REGISTRAR-SE</h2>
 
-            <p style="font-family: K2D" class="mt-10 flex pl-6 text-lg font-bold">Porte do veículo:</p>
-            <input type="password" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="Grande, Médio ou Pequeno">
+            <p style="font-family: K2D" class="mt-10 flex pl-6 text-lg font-bold">E-mail:</p>
+            <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="Usuario@gmail.com">
 
-            <p style="font-family: K2D" class="mt-2 flex pl-6 text-lg font-bold">Placa:</p>
-            <input type="password" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="000.000.000-00">
+            <p style="font-family: K2D" class="mt-2 flex pl-6 text-lg font-bold">Senha:</p>
+            <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="***********">
 
-            <button class="w-40 h-12 border mt-6 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">ENTRAR</button>
+            <p style="font-family: K2D" class="mt-2 flex pl-6 text-lg font-bold">Confirme sua senha:</p>
+            <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="***********">
+
+            <button class="w-40 h-12 border mt-6 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">CONTINUAR</button>
 
             <a class="cursor-pointer flex justify-self-center font-bold text-orange-400 mt-2" style="font-family: K2D">Já possui uma conta? Entrar</a>
         </div>
