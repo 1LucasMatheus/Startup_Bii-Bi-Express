7c991e69ca9144073ad876d1976ac115138c7fe5
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-03 13:06:46 -0300

    Criação do confirmar empresa e entregador

---
+++ resources/views/auth/confirmarEmpresaOuEntrgador.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    <!-- Confirmação do cadastro do entregador -->
+    <form action="" class="">
+    <div class="rounded-xl justify-self-center mt-13 text-center w-112 h-189 shadow-xl bg-neutral-100 pt-12">
+
+
+        <div class="border justify-self-center w-30 h-30 rounded-2xl font-bold"></div>
+       
+        <h2 class="justify-self-center text-2xl" style="font-family: K2D">Bii-Bi Express</h2>
+
+
+        <div class="rounded-xl shadow-xl w-90 h-107 justify-self-center mt-25 pt-2">
+
+
+            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">INFORMAÇÃO DO VEÍCULO</h2>
+
+
+            <p style="font-family: K2D" class="mt-10 flex pl-6 text-lg font-bold">Porte do veículo:</p>
+            <input type="password" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="Grande, Médio ou Pequeno">
+
+
+            <p style="font-family: K2D" class="mt-2 flex pl-6 text-lg font-bold">Placa:</p>
+            <input type="password" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="ABC-0000">
+
+
+            <button class="w-40 h-12 border mt-6 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">ENTRAR</button>
+
+
+            <a class="cursor-pointer flex justify-self-center font-bold text-orange-400 mt-2" style="font-family: K2D">Já possui uma conta? Entrar</a>
+        </div>
+
+
+    </div>
+    </form>
+
+
+    <!--- Confirmação do cadastro da empresa -->
+    <form action="" class="">
+    <div class="rounded-xl justify-self-center mt-13 text-center w-112 h-189 shadow-xl bg-neutral-100 pt-12">
+
+        <div class="border justify-self-center w-30 h-30 rounded-2xl font-bold"></div>
+        
+        <h2 class="justify-self-center text-2xl" style="font-family: K2D">Bii-Bi Express</h2>
+
+        <div class="rounded-xl shadow-xl w-90 h-87 justify-self-center mt-25 pt-2">
+
+            <h2 style="font-family: K2D" class="text-4xl font-bold mt-4">INFORME O INPJ</h2>
+
+            <p style="font-family: K2D" class="mt-10 flex pl-6 text-lg font-bold">INPJ:</p>
+            <input type="email" class="justify-self-center w-80 h-12 rounded-xl bg-neutral-100 text-gray-800 pl-2 shadow-2xl" style="font-family: K2D" placeholder="00.000.000/0000-00">
+
+            <button class="w-40 h-12 border mt-8 rounded-xl bg-neutral-800 text-slate-100 cursor-pointer">CADASTRAR</button>
+
+            <a class="cursor-pointer flex justify-self-center font-bold text-orange-400 mt-2" style="font-family: K2D">Já possui uma conta? Entrar</a>
+        </div>
+
+    </div>
+    </form>
+
+    ?>
+</body>
+</html>
