d538fc968e35b9b6028d77082982d919eb82f9d7
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-02 21:52:43 -0300

    Criação dos modos de cadastrar

---
+++ resources/views/modos_de_cadastro.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action="" class="">
+    <div class="w-30 h-30 rounded-xl justify-self-center mt-40 text-center w-107 h-138 shadow-xl bg-neutral-100 pt-12">
+
+        <div class="border justify-self-center w-30 h-30 rounded-2xl font-bold"></div>
+        
+        <h2 class="justify-self-center text-2xl" style="font-family: K2D">Bii-Bi Express</h2>
+
+        <button class="text-center justify-self-center mt-30 w-80 h-13 rounded-xl cursor-pointer bg-neutral-800 text-slate-50 text-2xl" style="font-family: nunito">ENTRAR</button>
+
+        <button class="text-center justify-self-center mt-10 w-80 h-13 rounded-xl cursor-pointer bg-neutral-800 text-slate-50 text-2xl" style="font-family: nunito">REGISTRAR-SE</button>
+
+        <a class="mt-14 cursor-pointer flex justify-self-center" style="font-family: K2D">Ajuda</a>
+    </div>
+    </form>
+</body>
+</html>
