d1b58633124aaa75b2c730b801a665457c5f78d8
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-04 11:08:58 -0300

    Começo do dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,36 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body>
+    <nav class="h-30 shadow bg-slate-900">
+
+      <h1 style="font-family: Agbalumo" class="text-slate-50 text-center flex pt-6 pl-6 text-6xl">Bii-bi Express</h1>
+
+    </nav>
+
+    <h3 style="font-family: Nunito" class="mt-10 text-4xl font-bold justify-self-center">Empresas parceiras</h3>
+
+    <div class="grid grid-col-3 justify-items-center pt-14 sm:grid grid-col-1 justify-items-center pt-12">
+      <div class="w-120 h-75 col-span-1 col-start-1 rounded-xl shadow-xl bg-gray-100"></div>
+      <div class="w-120 h-75 col-span-1 col-start-2 rounded-xl shadow-xl bg-gray-100"></div>
+      <div class="w-120 h-75 col-span-1 col-start-3 rounded-xl shadow-xl bg-gray-100"></div>
     </div>
-</x-app-layout>
+
+</body>
+</html>
